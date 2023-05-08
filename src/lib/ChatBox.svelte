0d7618db3f9d9e0c07059fa70bfb60cda605ae91
ArtemOsuskyi <a.osuskiy@gmail.com>
2023-05-09 02:00:50 +0300

    more messages

--- src/lib/ChatBox.svelte
+++ src/lib/ChatBox.svelte
@@ -1,5 +1,42 @@
 <script>
 	import Message from '$lib/Message.svelte';
+	import {Avatar} from "@skeletonlabs/skeleton";
+
+	export let messages = [
+		{
+			user: 'user',
+			text: 'yes'
+		},
+		{
+			user: 'other',
+			text: 'yes'
+		},
+		{
+			user: 'other',
+			text: 'yes'
+		},
+		{
+			user: 'user',
+			text: 'yes'
+		},
+		{
+			user: 'user',
+			text: 'yes'
+		},
+		{
+			user: 'other',
+			text: 'yes'
+		},
+		{
+			user: 'other',
+			text: 'yes'
+		},
+		{
+			user: 'user',
+			text: 'yes'
+		}
+	];
+	export let currentUser = 'user';
 </script>
 
 <div
@@ -11,12 +48,62 @@
 		Chat
 	</div>
 	<div
-		class="container max-w-[98%] h-full bg-chat-grey rounded-md relative"
+		class="container max-w-[98%] h-full bg-chat-grey rounded-md border border-amber-50 relative"
 	>
-		<Message />
+		<div class="messages min-h-full border-amber-50 border">
+			{#each messages as message}
+				<div class="message {message.user === currentUser ? 'user' : 'other'}">
+					{#if currentUser === 'user'}
+							<Avatar src="https://i.redd.it/0gk8b1l3maa31.png" />
+							<div class="text">{message.text}</div>
+						{:else }
+							<div class="text">{message.text}</div>
+							<Avatar src="https://i.redd.it/0gk8b1l3maa31.png" />
+						{/if}
+				</div>
+			{/each}
+		</div>
 	</div>
 	<input
 		class="input input-bordered h-auto w-full flex indent-2.5 pt-1.5 pb-1.5"
 		placeholder="Write a message..."
 	/>
 </div>
+
+<style>
+	.messages {
+		height: 400px;
+		overflow-y: scroll;
+		display: flex;
+		flex-direction: column-reverse;
+	}
+
+	.message {
+		margin-bottom: 8px;
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+	}
+
+	.message.user {
+		align-items: flex-end;
+	}
+
+	.message.user .text {
+		background-color: #ecf0f1;
+		color: #34495e;
+		border-radius: 16px 16px 0 16px;
+		padding: 8px 12px;
+		max-width: 300px;
+		text-align: right;
+	}
+
+	.message.other .text {
+		background-color: #bdc3c7;
+		color: #2c3e50;
+		border-radius: 16px 16px 16px 0;
+		padding: 8px 12px;
+		max-width: 300px;
+		text-align: left;
+	}
+</style>
--- src/lib/Message.svelte
+++ src/lib/Message.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
-    import {Avatar} from '@skeletonlabs/skeleton';
+	import { Avatar } from '@skeletonlabs/skeleton';
 </script>
 
-<div class="container flex border border-amber-50 w-[290px] h-auto bottom-[1px] absolute items-end">
-	<div class="h-[200px] w-[228px] place-self-end">
-		<Avatar src="https://i.redd.it/0gk8b1l3maa31.png"/>
+<div
+	class="container flex w-auto h-fit bottom-[1px] absolute border border-amber-50 gap-x-2.5"
+>
+	<div class="flex flex-col justify-end self-end">
+		<Avatar src="https://i.redd.it/0gk8b1l3maa31.png" />
 	</div>
-    <div class="container flex h-auto w-full border border-amber-50 gap-x-2">
-        Я люблю, когда волосатые мужики обмазываются маслом Я люблю, когда волосатые мужики обмазываются маслом Я люблю, когда волосатые мужики обмазываются маслом v Я люблю, когда волосатые мужики обмазываются маслом
-    </div>
+	<div
+		class="container h-auto w-[288px] text-start pl-2 bg-black rounded-md items-center justify-center"
+	>
+		Я люблю, когда волосатые мужики обмазываются маслом
+	</div>
 </div>
-
